073a6cc10e820212ff728abc43f6344cfd30ef89
rahul.x.ganesh <rahul.x.ganesh@192.168.1.2>
2026-08-09 14:27:18 +0530

    name change

--- src/test/java/seleniumBasics/seleniumBasicsTest.java
+++ src/test/java/seleniumBasics/seleniumBasicsTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class seleniumBasics {
+public class seleniumBasicsTest {
 
 	  @Test
 	    void viewBrowser(){
