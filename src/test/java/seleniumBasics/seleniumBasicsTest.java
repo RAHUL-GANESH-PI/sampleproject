51daf45eb0ae4500bdb21a43abcc50eda38a1775
rahul.x.ganesh <rahul.x.ganesh@192.168.1.2>
2026-08-09 14:38:37 +0530

    chromedriver

--- src/test/java/seleniumBasics/seleniumBasicsTest.java
+++ src/test/java/seleniumBasics/seleniumBasicsTest.java
@@ -4,16 +4,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class seleniumBasicsTest {
 
-	  @Test
-	    void viewBrowser(){
-	        ChromeDriver driver = new ChromeDriver();
-	        driver.get("https://www.netflix.com/in/");
-	        System.out.println(driver.getTitle());
-	        System.out.println(driver.getCurrentUrl());
-	        assertEquals("https://www.netflix.com/in/", driver.getCurrentUrl());
-	        driver.close();
-	    }
+	@Test
+	void viewChromeBrowser() {
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless=new");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		ChromeDriver driver = new ChromeDriver(options);
+		driver.get("https://www.netflix.com/in/");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		assertEquals("https://www.netflix.com/in/", driver.getCurrentUrl());
+		driver.close();
+	}
 }
