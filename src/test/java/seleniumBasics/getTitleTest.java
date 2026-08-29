7e9fc1f62ba2cc6d262505532a14749447eeda40
rahul <rahul@Rahul>
2026-08-29 13:55:53 +0530

    Amazon Entry

--- src/test/java/seleniumBasics/getTitleTest.java
+++ src/test/java/seleniumBasics/getTitleTest.java
@@ -2,24 +2,31 @@ package seleniumBasics;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class getTitleTest {
 
 	@Test
-	void viewChromeBrowser() {
+	void viewChromeBrowser() throws InterruptedException {
 
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--headless=new");
 		options.addArguments("--no-sandbox");
 		options.addArguments("--disable-dev-shm-usage");
-		ChromeDriver driver = new ChromeDriver(options);
-		driver.get("https://www.netflix.com/in/");
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in/");
+		driver.findElement(By.id("nav-hamburger-menu")).click();
+		Thread.sleep(2000);
 		System.out.println(driver.getTitle());
 		System.out.println(driver.getCurrentUrl());
-		assertEquals("https://www.netflix.com/in/", driver.getCurrentUrl());
+		assertEquals("https://www.amazon.in/", driver.getCurrentUrl());
 		driver.quit();
 	}
 }
